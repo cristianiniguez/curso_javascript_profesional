d01802db4df05f9094b303877ace7cd71ce5f945
Cristian Felix Iñiguez Porcel <felizpc1@gmail.com>
2020-08-24 19:51:18 -0400

    Convertir el proyecto a TypeScript

--- assets/index.ts
+++ assets/index.ts
@@ -1,10 +1,10 @@
-import MediaPlayer from './MediaPlayer.js'
-import AutoPlay from './plugins/AutoPlay.js'
-import AutoPause from './plugins/AutoPause.ts'
+import MediaPlayer from './MediaPlayer'
+import AutoPlay from './plugins/AutoPlay'
+import AutoPause from './plugins/AutoPause'
 
 const video = document.querySelector("video");
-const playpauseButton = document.querySelector("#play-pause");
-const muteunmuteButton = document.querySelector("#mute-unmute");
+const playpauseButton: HTMLElement = document.querySelector("#play-pause");
+const muteunmuteButton: HTMLElement = document.querySelector("#mute-unmute");
 
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
 
