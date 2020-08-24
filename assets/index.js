d267660d72ac770f4da96042e35668a4ea339d4d
Cristian Felix Iñiguez Porcel <felizpc1@gmail.com>
2020-08-24 14:18:13 -0400

    IntersectionObserver

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,12 @@
 import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
+import AutoPause from './plugins/AutoPause.js'
 
 const video = document.querySelector("video");
 const playpauseButton = document.querySelector("#play-pause");
 const muteunmuteButton = document.querySelector("#mute-unmute");
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
 
 playpauseButton.onclick = () => player.togglePlay();
 muteunmuteButton.onclick = () => player.toggleMute();
